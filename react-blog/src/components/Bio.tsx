307b13ce3f1b0c00ee23e0fbdc9c3187c7898ee6
Uni <1025687605@qq.com>
2019-01-09 23:56:13 +0800

    change

---
+++ react-blog/src/components/Bio.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './Bio.module.css'
+import bioPic from './bio-pic.jpg'
+
+interface BioProps {
+  className?: string
+}
+
+function Bio(props: BioProps) {
+  return (
+    <div
+      className={`
+      ${styles.Bio}
+      ${props.className || ''}
+    `}>
+      <img src={bioPic} alt="James K Nelson" />
+      <p>
+        Create a blog with a single command, by{' '}
+        <a href="https://twitter.com/james_k_nelson/">James K Nelson</a>.
+        <br />
+        Themed after Gatsby's blog starter and Dan Abramov's{' '}
+        <a href="https://overreacted.io/">overreacted.io</a>.<br />
+        Based on{' '}
+        <a href="https://facebook.github.io/create-react-app/">
+          create-react-app
+        </a>
+        , <a href="https://mdxjs.com/">MDX</a>, and{' '}
+        <a href="https://frontarm.com/navi/">Navi</a>.
+      </p>
+    </div>
+  )
+}
+
+export default Bio
